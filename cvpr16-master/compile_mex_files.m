8f8a13447ec996da6d9a7b41b1eb78ecc95f14ec
LeonShangguan <zhongkai0630@icloud.com>
2020-08-04 20:17:18 -0400

    Reference Code

---
+++ cvpr16-master/compile_mex_files.m
@@ -0,0 +1,10 @@
+% -g option of mex is necessary when you debug the code in C files
+mex mcmcShapeSampling.c
+mex -g mcmcShapeSampling.c
+
+mex EvolveWithDataTerm.c
+mex -g EvolveWithDataTerm.c
+
+mex evaluateEnergyWithShapePrior.c
+mex -g evaluateEnergyWithShapePrior.c
+
---
+++ cvpr16-master/untitled.m
@@ -0,0 +1,4 @@
+Psi = initialLevelSet(5, 5, 5);
+hold on,
+contour(Psi, [0 0], 'r'); % display initial curve on the figure
+hold off;
